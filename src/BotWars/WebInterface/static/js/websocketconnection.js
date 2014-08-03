1cf44a186de2efda7410ddf35619005871c5035d
Shyru <daniel@file-factory.de>
2014-08-03 19:09:17 +0200

    Implement very rough support for BotUpdatedMessages in the frontend. Looks not that bad for a first version!

--- src/BotWars/WebInterface/static/js/websocketconnection.js
+++ src/BotWars/WebInterface/static/js/websocketconnection.js
@@ -51,6 +51,10 @@ function onMessage(evt)
             }
             document.getElementById("playfield").innerHTML=imageHtml;
         }
+        else if (message.type=="BotUpdatedMessage")
+        {
+            handleBotUpdatedMessage(message);
+        }
     }
 
 }
@@ -65,6 +69,36 @@ function handleLogMessage(_logMessage)
     var targetMapping={
         "global":"globalLog"
     };
-    document.getElementById(targetMapping[_logMessage.logName]).innerHTML+=_logMessage.message+"\n";
+    document.getElementById(targetMapping[_logMessage.logName]).innerHTML+=_logMessage.message;
 
+}
+
+function handleBotUpdatedMessage(_botUpdatedMessage)
+{
+    var botId="bot-"+_botUpdatedMessage.id;
+    var botDom=document.getElementById(botId);
+    var top=_botUpdatedMessage.x*40+5-15+5;
+    var left=_botUpdatedMessage.y*40+5-15+5;
+    if (!botDom)
+    { //we got the first message of this bot, so we must add it
+        var botHtml='<div id="'+botId+'" class="bot">';
+        botHtml+='<div class="engine" style="background-image:url('+_botUpdatedMessage.engineImageUrl+');  transform: rotate('+_botUpdatedMessage.engineDirection+'deg); top:'+top+'px; left:'+left+'px;"></div>';
+        botHtml+='<div class="weapons" style="background-image:url('+_botUpdatedMessage.weaponsImageUrl+'); transform: rotate('+_botUpdatedMessage.weaponDirection+'deg); top:'+top+'px; left:'+left+'px;""></div>';
+
+        botHtml+="</div>";
+        document.getElementById("playfield").innerHTML+=botHtml;
+    }
+    else
+    { //we must update the dom
+        var engine=botDom.querySelector(".engine");
+        var weapons=botDom.querySelector(".weapons");
+        engine.style.transform="rotate("+_botUpdatedMessage.engineDirection+'deg)';
+        engine.style.top=top+"px";
+        engine.style.left=left+"px";
+
+        weapons.style.transform="rotate("+_botUpdatedMessage.weaponDirection+'deg)';
+        weapons.style.top=top+"px";
+        weapons.style.left=left+"px";
+
+    }
 }
